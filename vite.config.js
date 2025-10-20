15fea2f58227360c66755780e519a8c122e7b596
Cristianrage <cristianrage1@gmail.com>
2025-10-20 20:59:28 -0300

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🚨 ¡IMPORTANTE! Agrega la propiedad 'base' con el nombre de tu repositorio
+  // Reemplaza 'nombre-de-tu-repositorio' por el nombre real de tu repo en GitHub.
+  base: 'tech-talent-react-project', 
 })
